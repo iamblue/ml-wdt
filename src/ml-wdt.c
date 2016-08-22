1abf2744cb2c5ca78b1faa0b17b84c67af9fe805
po-ju.chen <tonyone0902@gmail.com>
2016-08-22 14:43:30 +0800

    upgrade to new jerry api

--- src/ml-wdt.c
+++ src/ml-wdt.c
@@ -1,30 +1,28 @@
 #include <string.h>
 
 #include "microlattice.h"
-
-#include "jerry.h"
-
+#include "jerry-api.h"
 #include "hal.h"
 
 DELCARE_HANDLER(__wdtInit)
 {
   hal_wdt_config_t wdt_init;
   wdt_init.mode = HAL_WDT_MODE_RESET;
-  wdt_init.seconds = (int)args_p[0].v_float32;
+  wdt_init.seconds = jerry_get_number_value(args_p[0]);
   hal_wdt_init(&wdt_init);
-  return true;
+  return jerry_create_boolean (true);
 }
 
 DELCARE_HANDLER(__wdtEnable)
 {
   hal_wdt_enable(HAL_WDT_ENABLE_MAGIC);
-  return true;
+  return jerry_create_boolean (true);
 }
 
 DELCARE_HANDLER(__wdtFeed)
 {
   hal_wdt_feed(HAL_WDT_FEED_MAGIC);
-  return true;
+  return jerry_create_boolean (true);
 }
 
 void ml_wdt_init(void)
